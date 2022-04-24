e5dd5f64569ec2d4174212d8bd31b5df595ca198
Sean Young <seanyoung247@gmail.com>
2022-04-24 23:47:00 +0100

    refactor: Moves object code to seperate script

---
+++ assets/js/objects.js
@@ -0,0 +1,195 @@
+
+let issues = 0;
+
+function incCount(obj, amt = 1) {
+    obj.triggered = !obj.triggered;
+    issues += obj.triggered ? amt : -amt;
+    console.log(issues,obj.triggered);
+} 
+
+const dishes = {
+    triggered: false,
+    do: function() {
+        const dishes = document.getElementById('kitchen-dishes');
+        dishes.classList.toggle('disabled');
+
+        incCount(this, 2);
+        //TO DO: Show information dialog
+    }
+};
+
+const cooking = {
+    triggered: false,
+    do: function() {
+        incCount(this, 2);
+        //TO DO: Show information dialog
+    }
+};
+
+const actions = {
+    'power-station-trigger': {
+        triggered: false,
+        do: function() {
+            const coalStation = document.getElementById('power-plant');
+            const windmills = document.getElementById('windmills');
+            coalStation.classList.toggle('disabled');
+            windmills.classList.toggle('disabled');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    'attic-object': {
+        triggered: false,
+        do: function() {
+            const insulation = document.getElementById('loft-insulation');
+            insulation.classList.toggle('disabled');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    // Kitchen
+    "fridge-outline": {
+        triggered: false,
+        do: function() {
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "kettle-outline": {
+        triggered: false,
+        do: function() {
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "kitchen-tap-outline": {
+        triggered: false,
+        do: function() {
+            const water = document.getElementById('kitchen-sink-water');
+            const stream = document.getElementById('kitchen-water-line');
+            water.classList.toggle('disabled');
+            stream.classList.toggle('disabled');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "kitchen-sink-outline": dishes,
+    "dishwasher-outline": dishes,
+
+    "microwave-outline": cooking,
+    "oven-outline": cooking,
+
+    // Living room
+    'livingroom-lights-outline': {
+        triggered: false,
+        do: function() {
+            const blackout = document.getElementById('livingroom-blackout');
+            blackout.classList.toggle('disabled');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "tv-outline": {
+        triggered: false,
+        do: function() {
+            const tvLed = document.getElementById('power-led');
+            tvLed.classList.toggle('off');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    // Bedroom
+    'bedroom-lights-outline': {
+        triggered: false,
+        do: function() {
+            const blackout = document.getElementById('bedroom-blackout');
+            blackout.classList.toggle('disabled');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "covers-outline": {
+        triggered: false,
+        do: function() {
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "socket-outline": {
+        triggered: false,
+        do: function() {
+            const charger = document.getElementById('charger');
+            charger.classList.toggle('disabled');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    // Bathroom
+    "toilet-outline": {
+        triggered: false,
+        do: function() {
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "toilet-paper-outline": {
+        triggered: false,
+        do: function() {
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "bathroom-tap-outline": {
+        triggered: false,
+        do: function() {
+            const water = document.getElementById('bathroom-sink-water');
+            const stream = document.getElementById('bathroom-sink-water-line');
+            water.classList.toggle('disabled');
+            stream.classList.toggle('disabled');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "shower-outline": {
+        triggered: false,
+        do: function() {
+            const water = document.getElementById('water-stream');
+            const stream = document.getElementById('water-lines');
+            console.log(water);
+            water.classList.toggle('disabled');
+            stream.classList.toggle('disabled');
+
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    },
+
+    "washing-machine-outline": {
+        triggered: false,
+        do: function() {
+            incCount(this);
+            //TO DO: Show information dialog
+        }
+    }
+}
