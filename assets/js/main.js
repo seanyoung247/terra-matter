813f2a20737bc869cc450294827c59f230386ee9
Sean Young <seanyoung247@gmail.com>
2022-04-25 02:03:58 +0100

    feat: Adds initial modal code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,40 @@
 
-const graphic = document.getElementById('house-svg');
-const objects = document.querySelectorAll('.object-outline,#power-station-trigger');
-const objectCount = objects.length;
-/**
- * state is a value between 0.0 and 1.0 reflecting how many objects have been activated.
- * Where 0.0 is 0% and 1.0 is 100%.
+/*
+ * Page setup and state management
  */
-let state = 0.0;
+(function() {
+    const graphic = document.getElementById('house-svg');
+    const objects = document.querySelectorAll('.object-outline,#power-station-trigger');
+    const objectCount = objects.length;
+    /**
+     * state is a value between 0.0 and 1.0 reflecting how many objects have been activated.
+     * Where 0.0 is 0% and 1.0 is 100%.
+     */
+    let state = 0.0;
 
-/**
- * Updates state with the current number of activated objects.
- * @param {Number} count - The current number of activated objects
- */
-function updateState(count) {
-    state = count / objectCount;
-    console.log(state);
-    graphic.style.setProperty('--mix', state);
-}
-
-function objectClick() {
-    if (actions[this.id]) {
-        actions[this.id].do(updateState);
+    /**
+     * Updates state with the current number of activated objects.
+     * @param {Number} count - The current number of activated objects
+     */
+    function updateState(count) {
+        state = count / objectCount;
+        console.log(state);
+        graphic.style.setProperty('--mix', state);
     }
-}
-for (const object of objects) {
-    object.addEventListener('click', objectClick);
-}
+
+    function objectClick() {
+        if (actions[this.id]) {
+            actions[this.id].do(updateState);
+        }
+    }
+    for (const object of objects) {
+        object.addEventListener('click', objectClick);
+    }
+})();
+
+/*
+ * Modals
+ */
+(function() {
+    const modal = new bootstrap.Modal(document.getElementById('info-modal'));
+})();
